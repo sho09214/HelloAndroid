e625de83021a470fc15c72c8a6e066ecda9f18e8
sho09214 <motohashi.sho@gmail.com>
2025-07-08 06:27:56 +0900

    Add - Section5

--- app/src/main/java/com/websarva/wings/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/helloandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package com.websarva.wings.android.helloandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //ListViewオブジェクトを取得
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        //ListViewにリスナを設定
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    //リストがタップされたときの処理が記述されたメンバクラス
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //タップされた定食名を取得
+            String item = (String) parent.getItemAtPosition(position);
+            //トーストで表示する文字列を生成
+            String show = "あなたが選んだ定食：" + item;
+            //トーストの表示
+            Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+        }
     }
 }
